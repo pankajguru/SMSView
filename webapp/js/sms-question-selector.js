e5be772d15f69ffa8d27a3dbfdbf3fa8cfd78c57
Rory Zweistra <rory@ryuu.nl>
2012-01-11 14:07:46 +0100

    Finished code for clickable categories

--- webapp/js/sms-question-selector.js
+++ webapp/js/sms-question-selector.js
@@ -82,7 +82,7 @@ function retrieve_questions_per_type( type ) {
   		success: function(xml){
     		$(xml).find('item').each(function() {
     			
-    			var li = '<li title="' + $(this).find('category_nam').text() + '" class="ui-state-default hide" id="' + $(this).find('question_id').text() + '">' + $(this).find('question_description').text() + '</li>';
+    			var li = '<li title="' + $(this).find('category_name').text() + '" class="ui-state-default hide" id="' + $(this).find('question_id').text() + '">' + $(this).find('question_description').text() + '</li>';
     			$(li).appendTo('#questions_container');
     		});
     		sort_on_category();
@@ -155,28 +155,32 @@ function sort_on_category() {
     	var title		= $( this ).attr('title');
     	var li_group	= 'list' + $(this).attr('title');
 
-		if( !groups[ li_group ] )
+		if( !groups[ li_group ] ) {
       		groups[ li_group ] = [];
-    		groups[ li_group ].push( li );
-  		});
+      		var first_li = $('<li title="' + title + '" class="category_name" />').text( title );
+      		groups[ li_group ].push( first_li );
+      	}
+      		groups[ li_group ].push( li );
+	});
   
- 		for(group in groups){
-			var ul = $('<ul class="connectedSortable ui-sortable sorts" />').attr( 'id', group );
-			var first_li = $('<li class="category_title" />').text( title );
-			ul.append( first_li );
-    		var lis = groups[group];
+ 	for( group in groups ) {
+		var ul = $('<ul class="connectedSortable ui-sortable sorts" />').attr( 'id', group );
+    	var lis = groups[ group ];
     
-			for( i = 0; i < lis.length; i++ ){
-      			ul.append( lis[ i ] );
-    		}
-    		ul.appendTo('#questions_container');
-    		
-    		$(ul).click( function() {
-    			$(this + '> li' ).toggleClass('hide');	
-    		});
-    		
-  		}
-};
+		for( i = 0; i < lis.length; i++ ){
+    		ul.append( lis[ i ] );
+    	}
+    	ul.appendTo('#questions_container');
+  	}
+  	
+  	create_clicks();
+}
+
+function create_clicks() {
+	$('.category_name').click( function() {
+		$( this ).parent().children('.ui-state-default').toggleClass('hide');
+	});
+}
 
 //function sort_on_category( a, b ) {
 //    return $(a).attr('title').toLowerCase() > $(b).attr('title') ? 1 : -1;
