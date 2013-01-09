41c4a8722b70c26d0dbd52627cb389a6be936ece
foo <foo@sms.scholenmetsucces.nl>
2013-01-09 13:58:07 +0100

    added config

---
+++ application/config/sms-pollux.php
@@ -0,0 +1,4 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['report_dirs']	= array('muis' => '/data/MUIS');
+$config['vragenplanner_url']	= 'http://192.168.0.250/index.php/';
